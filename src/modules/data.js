52ac079843dd702a54a052a336c151b0b0681040
wayungi <fwayungi@gmail.com>
2022-07-19 23:57:19 +0300

    Add ui.js file

---
+++ src/modules/data.js
@@ -0,0 +1,18 @@
+const nameScore = [
+  { name: 'Francis', score: 100 },
+  { name: 'James', score: 93 },
+  { name: 'Peter', score: 97 },
+  { name: 'Bosco', score: 80 },
+  { name: 'Okla', score: 87 },
+  { name: 'Mich', score: 71 },
+  { name: 'Wayungi', score: 75 },
+  { name: 'Cekcan', score: 89 },
+  { name: 'Omara', score: 45 },
+  { name: 'Owino', score: 74 },
+  { name: 'Karuhanga', score: 45 },
+  { name: 'Galikuki', score: 74 },
+];
+
+const getNameScores = () => nameScore;
+
+export default getNameScores;
