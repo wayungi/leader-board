67d6c82e9b3a021ec83041c560797b74a55be75a
wayungi <fwayungi@gmail.com>
2022-07-19 20:23:04 +0300

    Configure webpack-config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Leader board',
+        template: './src/index.html',
+      }),
+    ],
+
     output: {
-      filename: 'main.js',
+      filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
+      clean:true,
     },
   };
