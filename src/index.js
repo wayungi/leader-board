3592433bf9e254863533454d8d4754951c19c95b
wayungi <fwayungi@gmail.com>
2022-07-19 23:49:41 +0300

    Add method to add name-scores to display

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './style.css';
+import getNameScores from './modules/data.js';
+import displayNameScore from './modules/ui.js';
 
+const scoreDisplay = document.getElementById('scores-list');
+
+getNameScores().forEach((person) => {
+  displayNameScore(person.name, person.score, scoreDisplay);
+});
