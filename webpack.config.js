f5d3e4b6d6394d29ced96fdc55e8d89f8cec111b
wayungi <fwayungi@gmail.com>
2022-07-19 21:53:48 +0300

    Fix elsint errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devServer: {
-        static: './dist',
-    },
-    
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Leader board',
-        template: './src/index.html',
-      }),
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leader board',
+      template: './src/index.html',
+    }),
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
+  },
 
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean:true,
-    },
-
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
-      },
-
-      optimization: {
-        runtimeChunk: 'single',
-      },
-  };
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
