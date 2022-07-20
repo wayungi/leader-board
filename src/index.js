113ff29956426675b497721a62a127554d40a0ae
wayungi <fwayungi@gmail.com>
2022-07-20 22:10:52 +0300

    Create new game on page load

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import { getNameScores, createGame } from './modules/data.js';
 import displayNameScore from './modules/ui.js';
 
 const scoreDisplay = document.getElementById('scores-list');
-c
+const refresh = document.getElementById('refresh-btn');
+let gameId = '';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  gameId = await createGame();
+});
+
+
 
 getNameScores().forEach((person) => {
   displayNameScore(person.name, person.score, scoreDisplay);
