10ad81cf7abdd1eb98c4bddacff8e98aa9d829ba
wayungi <fwayungi@gmail.com>
2022-07-20 19:54:48 +0300

    Add create game method

--- src/modules/data.js
+++ src/modules/data.js
@@ -15,4 +15,21 @@ const nameScore = [
 
 const getNameScores = () => nameScore;
 
-export default getNameScores;
+const createGame = async () => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: 'Tuku kweyo',
+      }),
+    }
+  );
+  const result = await response.text();
+  return result;
+};
+
+export { getNameScores, createGame };
