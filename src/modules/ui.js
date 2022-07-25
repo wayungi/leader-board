de1f63fa958f291fa6034aed977de2f1caf726c0
wayungi <fwayungi@gmail.com>
2022-07-25 09:55:49 +0300

    Add method to create user profile icon

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,3 +1,9 @@
+const userIcon = () => {
+  const user = document.createElement('div');
+  user.innerHTML = '<i class="fa-solid fa-user"></i>';
+  return user;
+};
+
 const displayNameScore = (name, score, pageElement) => {
   const nameScoreParagraph = document.createElement('p');
   nameScoreParagraph.innerText = `${name}:${score}`;
